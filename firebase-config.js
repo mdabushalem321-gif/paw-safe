8b3cbbc4208c0ece34cbc8ab8b69c69cab91faeb
mdabushalem321-gif <mdabushalem321@gmail.com>
2026-04-07 22:10:04 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
@@ -20,5 +21,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { app, analytics, db, storage, collection, addDoc, getDocs, query, orderBy, limit, onSnapshot, ref, uploadBytes, getDownloadURL, getAuth };
+export { app, analytics, db, storage, auth, collection, addDoc, getDocs, query, orderBy, limit, onSnapshot, ref, uploadBytes, getDownloadURL };
